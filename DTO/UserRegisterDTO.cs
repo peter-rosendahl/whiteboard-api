b25ffc78d5b4b5845524316fb349c0a180220f3a
Peter Rosendahl <p.rosendahl.89@gmail.com>
2020-12-12 15:28:02 +0000

    v1.2.0 new endpoint (bool) UserExists.

--- DTO/UserRegisterDTO.cs
+++ DTO/UserRegisterDTO.cs
@@ -2,13 +2,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Whiteboard.API.DTO
 {
-    public class UserRegisterDTO
+    public class UserRegisterDTO : UserLoginDTO
     {
-        [Required]
-        public string Username { get; set; }
-        [Required]
-        [StringLength(16, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 16 characters.")]
-        public string Password { get; set; }
         public string Email { get; set; }
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # Whiteboard API
-*v.1.1.0*
+*v.1.2.0*
 
 > Also the API for My Pocket Notes mobile app.
 
 **The description of this API will be applied at a later session.**
 
 # Changelog
+
+## [1.2.0] 12/12/2020
+
+### Added Features
+- New Endpoint Users -> bool UserExists(string username)
+
+### Fixes
+- Users -> Login returns statusCodes 401 and 404 if user doesn't exist or password is incorrect.
 
 ## [1.1.0] 08/12/2020
 
